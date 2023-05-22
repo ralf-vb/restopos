be529e2ec293a0eb68ffc025b36fcd6c976fa7ff
Intern-BDerequito <intern-bderequito@FDSAP-Admins-MacBook-Air.local>
2023-05-22 15:01:00 +0800

    screens added

--- lib/screens/admin/pospage.dart
+++ lib/screens/admin/pospage.dart
@@ -1,12 +1,206 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class pospage extends StatelessWidget {
+class Product {
+  String name;
+  String imagePath;
+  double price;
+  String description;
+
+  Product({
+    required this.name,
+    required this.imagePath,
+    required this.price,
+    required this.description,
+  });
+}
+
+class pospage extends StatefulWidget {
   const pospage({Key? key}) : super(key: key);
+
+  @override
+  _pospageState createState() => _pospageState();
+}
+
+class _pospageState extends State<pospage> {
+  List<Product> products = [];
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  void addProduct() {
+    setState(() {
+      final newProduct = Product(
+        name: nameController.text,
+        imagePath: _image?.path ?? '',
+        price: double.parse(priceController.text),
+        description: descriptionController.text,
+      );
+      products.add(newProduct);
+      // Clear the input fields
+      nameController.clear();
+      priceController.clear();
+      descriptionController.clear();
+      _image = null;
+    });
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = pickedFile;
+    });
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    priceController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: _image != null
+                ? Image.file(
+              _image!.path,
+              width: 50.0,
+              height: 50.0,
+            )
+                : null,
+            title: Text(products[index].name),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Price: \$${products[index].price.toStringAsFixed(2)}'),
+                Text('Description: ${products[index].description}'),
+              ],
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Edit Product'),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ElevatedButton(
+                            onPressed: _pickImage,
+                            child: Text('Select Image'),
+                          ),
+                          TextField(
+                            controller: priceController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Price',
+                            ),
+                          ),
+                          TextField(
+                            controller: descriptionController,
+                            decoration: InputDecoration(
+                              labelText: 'Description',
+                            ),
+                          ),
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              products[index].imagePath = _image?.path ?? '';
+                              products[index].price =
+                                  double.parse(priceController.text);
+                              products[index].description =
+                                  descriptionController.text;
+                              Navigator.of(context).pop();
+                            });
+                          },
+                          child: Text('Update'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Add Product'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _pickImage,
+                      child: Text('Select Image'),
+                    ),
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                      ),
+                    ),
+                    TextField(
+                      controller: priceController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Price',
+                      ),
+                    ),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: InputDecoration(
+                        labelText: 'Description',
+                      ),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      addProduct();
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
